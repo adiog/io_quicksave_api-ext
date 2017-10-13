5778a2468e723c119b452df89a56739fb88c9fad
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-13 04:21:47 +0200

    init

--- src/quicksave_api/PythonBeanAPI.py
+++ src/quicksave_api/PythonBeanAPI.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
 from PluginEngine import main
-
+from quicksave_pybeans.generated.QsBeans import InternalCreateRequestBean
 
 def process(internalCreateRequestBean):
     return main(internalCreateRequestBean)
