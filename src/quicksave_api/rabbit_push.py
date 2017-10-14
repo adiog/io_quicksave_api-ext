eed933d0f7bd3b8768f708990941fadad680ac12
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-14 10:24:34 +0200

    env

--- src/quicksave_api/rabbit_push.py
+++ src/quicksave_api/rabbit_push.py
@@ -2,7 +2,7 @@ import pika
 import quicksave_api.env
 
 def rabbit_push(queue, bean):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=env.IO_QUICKSAVE_MQ_HOST, port=env.IO_QUICKSAVE_MQ_PORT))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=quicksave_api.env.IO_QUICKSAVE_MQ_HOST, port=quicksave_api.env.IO_QUICKSAVE_MQ_PORT))
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange='',
