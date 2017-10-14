6a78c1d681e6d6c6d5d0f6e7d0b67546963b4db9
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-14 10:10:55 +0200

    fix paths

--- src/quicksave_api/PythonBeanAPI.py
+++ src/quicksave_api/PythonBeanAPI.py
@@ -1,7 +1,7 @@
 # This file is a part of quickave project.
 # Copyright (c) 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
-from PluginEngine import main
+from quicksave_api.PluginEngine import main
 from quicksave_pybeans.generated.QsBeans import InternalCreateRequestBean
 from quicksave_pybeans.pybeans import to_string
 
