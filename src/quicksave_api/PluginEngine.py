5cb21750a6bc1ff1dfb325270ddaaf4ce3e0cb37
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-14 10:21:00 +0200

    do not print

--- src/quicksave_api/PluginEngine.py
+++ src/quicksave_api/PluginEngine.py
@@ -14,20 +14,16 @@ from quicksave_api.rabbit_push import rabbit_push
 def main(internalCreateRequestBean):
     createRequestBean = internalCreateRequestBean.createRequest
     metaBean = createRequestBean.meta
-    print(metaBean.to_json())
     if metaBean.meta_type == 'image':
         rabbit_push('request', BackgroundTaskBean(name='image', internalCreateRequest=internalCreateRequestBean, kwargs='{}'))
     if metaBean.meta_type == 'page':
         rabbit_push('request', BackgroundTaskBean(name='thumbnail', internalCreateRequest=internalCreateRequestBean, kwargs='{}'))
         rabbit_push('request', BackgroundTaskBean(name='wget', internalCreateRequest=internalCreateRequestBean, kwargs='{}'))
-    print(metaBean.source_url)
     tags = [TagBean(name='python_sync')]
     if metaBean.source_url is not None:
         if 'youtube.com' in metaBean.source_url:
-            print('youtube')
             rabbit_push('request', BackgroundTaskBean(name='youtube', internalCreateRequest=internalCreateRequestBean, kwargs='{}'))
         if 'github.com' in metaBean.source_url:
-            print('github')
             rabbit_push('request', BackgroundTaskBean(name='git', internalCreateRequest=internalCreateRequestBean, kwargs='{}'))
         if ('wikipedia' in metaBean.source_url):
             tags.append(TagBean(name='wiki'))
